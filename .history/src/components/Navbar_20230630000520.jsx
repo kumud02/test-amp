f3595a2cc3df6a81c9fb04b20e732a5eb8e248f9
Kumud Kharel <kumud@rvlife.com>
2023-06-30 12:25:46 +0545

    home page changes

---
+++ .history/src/components/Navbar_20230630000520.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../img/testamplify-logo-white.png";
+
+
+
+const Navbar = () => {
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+      <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <a href="https://testamplify.net/" rel="home">
+              <img src={logo} alt="TestAmplify Academy" className="logo-img" />
+            </a>
+          </NavLink>
+        <button className="navbar-toggler custom-toggler" onClick={openModal} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink exact activeClassName="menu_active" className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/service">Services</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/contact">Contact</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/faq">FAQ</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+
+
+
+
+
+
+</>
+  );
+};
+
+export default Navbar;
---
+++ .history/src/components/Navbar_20230630103107.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../img/testamplify-logo-white.png";
+import logo2 from "../img/lop"
+import { useState } from "react";
+
+
+
+const Navbar = () => {
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light  bg-transparent">
+      <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <a href="https://testamplify.net/" rel="home">
+              <img src={logo} alt="TestAmplify" className="logo-img" />
+            </a>
+          </NavLink>
+        <button 
+          className="navbar-toggler ml-auto bg-light custom-toggler" 
+          type="button" 
+          // data-bs-toggle="collapse" 
+          // data-bs-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+          onClick={openModal} 
+
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink exact activeClassName="menu_active" className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/service">Services</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/contact">Contact</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="menu_active" className="nav-link" to="/faq">FAQ</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+     {/* Bootstrap Modal */}
+     <div
+        className={`modal fade ${modalOpen ? "show" : ""}`}
+        style={{ display: modalOpen ? "block" : "none" }}
+        tabIndex="-1"
+        aria-hidden={!modalOpen}
+        onClick={closeModal}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal Title</h5>
+              <button 
+                type="button" 
+                className="btn-close" onClick={closeModal}></button>
+            </div>
+            <div className="modal-body">
+            <NavLink className="navbar-brand" to="/">
+            <a href="https://testamplify.net/" rel="home">
+              <img src={logo} alt="TestAmplify" className="logo-img" />
+            </a>
+          </NavLink>
+              
+            <NavLink to="/contact" className="btn-get-consultation home-text">
+                        Request Consultation
+                      </NavLink>
+                      <NavLink to="/contact" className="btn-get-consultation home-text">
+                        Request Consultation
+                      </NavLink>
+              <p>Modal Content</p>
+
+              ashdsakdhaskdhksja asdhgsadhsjkah
+            
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+</>
+  );
+};
+
+export default Navbar;
